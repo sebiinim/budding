1296fc5fac312b6997113c13eae1ebcb81346337
sebin <1219sebin@gmail.com>
2025-11-23 20:53:55 +0900

    build.gradle.ktx에서 버전 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,17 +25,18 @@ repositories {
 }
 
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
 
-    implementation("org.postgresql:postgresql:42.6.0")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
